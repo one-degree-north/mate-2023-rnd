e0984ce9fe065259aa144a4538e34c9205788edd
Alan Chen <alan.chen.2k6@gmail.com>
2023-03-26 16:49:06 +0800

    changed max_thrust and max delta again

--- firmware/v3/main-tube/firmware.c
+++ firmware/v3/main-tube/firmware.c
@@ -461,7 +461,9 @@ void setup_outputs() {
 void loop_outputs() {
     u64 now = to_us_since_boot(get_absolute_time());
     u64 elapsed_us = thruster_prev_loop_us - now;
-    u16 max_delta = MIN(MAX_DELTA_POS * elapsed_us / 5000000, 0xDFFF);
+    u16 max_delta = MIN(MAX_DELTA_POS * elapsed_us / 1000000000, 0xFFFF);
+    // old max_delta calculation
+    // u16 max_delta = MIN(MAX_DELTA_POS * elapsed_us / 1000000, 0xFFFF);
 
     // set each output to current value
     for (int i = 0; i < NUM_THRUSTERS; ++i) {
