970679cdaacadd59ba7750ba66586050482d73fa
lutet88 <xhex88jeff@gmail.com>
2023-04-18 14:22:24 +0800

    firmware-v4-refactor: remove debug loop, add config

--- firmware/v4/opi/pi_main.py
+++ firmware/v4/opi/pi_main.py
@@ -3,31 +3,41 @@ from server import OPiServer
 from thruster_control import ThrusterController
 from data import OpiDataProcess
 import threading
+import json
 
 if __name__ == "__main__":
-    # there is a circular dependency with: thrust_controller requiring data and interface, server requiring thrust_controller, and data requiring server
-    # addr = input("server address> ")
-    # serial_port = input("serial port > ")
-    addr = "192.168.13.103"
-    serial_port = "/dev/ttyS5"
-    thrust_controller = ThrusterController()
-    server = OPiServer((addr, 7772))
-    opi_data = OpiDataProcess()
+    # setup configuration file contents
+    config_file = open('config.json', 'r')
+    config = json.loads(config_file.read())
+    config_file.close()
+
+    # create components
+    addr = config['ip_addr']
+    serial_port = config['serial_port']
+    thrust_controller = ThrusterController(config['thruster_move_delta'], not not config['debug'])
+    server = OPiServer((addr, config['udp_port']))
     interface = MCUInterface(serial_port)
-    # resolve dependencies
+
+    # resolve dependencies between components
     thrust_controller.set_interface(interface)
-    thrust_controller.set_data(opi_data)
     server.set_thruster_control(thrust_controller)
     server.set_interface(interface)
-    opi_data.set_server(server)
     interface.set_server(server)
-    #start stuff!
+
+    # config if we are using bno data
+    if config['use_bno']:
+        opi_data = OpiDataProcess()
+        thrust_controller.set_data(opi_data)
+        opi_data.set_server(server)
+
+    # start the system
     print("starting server")
     server.start_server()
     print("starting interface")
     interface.start()
-    print("starting BNO")
-    opi_data.start_bno_reading()
+    if config['use_bno']:
+        print("starting BNO")
+        opi_data.start_bno_reading()
     print("starting thruster controller")
     thrust_controller.start_loop()
     server.server_thread.join()
